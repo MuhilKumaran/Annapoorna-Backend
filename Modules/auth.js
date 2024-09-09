bb3d4e6b7546eb752f7284dd4d93b94b15df7e0f
MuhilKumaran <137814562+MuhilKumaran@users.noreply.github.com>
2024-09-09 22:03:47 +0530

    Update auth.js

--- Modules/auth.js
+++ Modules/auth.js
@@ -22,11 +22,34 @@ exports.authenticateAdmin = (req, res, next) => {
     next();
   });
 };
+// exports.authenticateCustomer = (req, res, next) => {
+//   console.log("Auth middle ware");
+//   console.log(req.cookies);
+//   const token = req.cookies.token;
+//   console.log(token);
+//   if (!token) {
+//     return res.status(401).json({ message: "Login to view the order" });
+//   }
+
+//   jwt.verify(token, SECRET_KEY, (err, user) => {
+//     if (err) {
+//       return res.status(403).json({ message: "Invalid token" });
+//     }
+//     req.user = user; // Attach user data to the request object
+//     console.log(user);
+//     if (user.role !== "customer")
+//       return res.status(403).json({ message: "Access denied. customer only." });
+//     next();
+//   });
+// };
+
 exports.authenticateCustomer = (req, res, next) => {
-  console.log("Auth middle ware");
-  console.log(req.cookies);
-  const token = req.cookies.token;
-  console.log(token);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Login to view the order" });
+  }
+
+  const token = authHeader.split(' ')[1]; // Extract the token
   if (!token) {
     return res.status(401).json({ message: "Login to view the order" });
   }
@@ -37,8 +60,10 @@ exports.authenticateCustomer = (req, res, next) => {
     }
     req.user = user; // Attach user data to the request object
     console.log(user);
-    if (user.role !== "customer")
-      return res.status(403).json({ message: "Access denied. customer only." });
+    if (user.role !== "customer") {
+      return res.status(403).json({ message: "Access denied. Customers only." });
+    }
     next();
   });
 };
+
